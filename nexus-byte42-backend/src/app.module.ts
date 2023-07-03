1e0b9fe1b0272030c28c5ee6e71c30743010e094
Burak <burakgumusfb@gmail.com>
2023-07-03 17:37:08 +0300

    docker compose.

--- nexus-byte42-backend/src/app.module.ts
+++ nexus-byte42-backend/src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({ envFilePath: `.env` }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTTION),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
