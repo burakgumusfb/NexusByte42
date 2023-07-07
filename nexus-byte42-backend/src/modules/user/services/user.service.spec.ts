e4c98c88d4aa2cf658178f1acc0c933630781aa6
Burak <burakgumusfb@gmail.com>
2023-07-07 14:09:18 +0300

    user service unit test.

--- nexus-byte42-backend/src/modules/user/services/user.service.spec.ts
+++ nexus-byte42-backend/src/modules/user/services/user.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { CustomConfigModule } from '@app/config/custom-config.module';
+import { SchemaModule } from '@app/schemas/schema.module';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CustomConfigModule, SchemaModule],
       providers: [UserService],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('insert', () => {
+    it('should return user when new user created', async () => {
+      const email = 'test@example.com';
+      const password = 'password123';
+      const result = await userService.insert(email, password);
+      expect(result.email).toBe(email);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return user when email and password are valid', async () => {
+      const email = 'test@example.com';
+      const password = 'password123';
+      const result = await userService.findOne(email, password);
+      expect(result.email).toBe(email);
+    });
   });
 });
--- nexus-byte42-backend/src/modules/user/services/user.service.ts
+++ nexus-byte42-backend/src/modules/user/services/user.service.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/schemas/user.schema';
+import { User } from '@schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
-import { SALT_OR_ROUNDS } from 'src/common/constants/user.constant';
+import { SALT_OR_ROUNDS } from '@common/constants/user.constant';
 
 @Injectable()
 export class UserService {
@@ -14,7 +14,6 @@ export class UserService {
   ) { }
 
   async findOne(email: string, password: string): Promise<User> {
-
     const user = await this.userModel.findOne({ email: email }).lean();
     if (user) {
       const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -24,9 +23,10 @@ export class UserService {
     }
     return null;
   }
-  async insert(email: string, password: string) {
+  async insert(email: string, password: string): Promise<User> {
     const hashPassword = await bcrypt.hash(password, SALT_OR_ROUNDS);
     const employee = new this.userModel({ email: email, password: hashPassword });
     await employee.save();
+    return employee;
   }
 }
